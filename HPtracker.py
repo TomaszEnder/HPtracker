c21bcdd81d47f62cefdaa7bd47d2b5fb891dc0d9
TomaszEnder <114151278+TomaszEnder@users.noreply.github.com>
2022-09-22 12:52:03 +0200

    Main file and readme update
    HP tracker uploaded and readme updated

---
+++ HPtracker.py
@@ -0,0 +1,99 @@
+class Mob:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def hp_set(self):
+        print(self.name + " hp has been set.")
+
+    def oof(self):
+        damage = int(input("Enter damage: "))
+        self.hp -= damage
+
+    def results(self):
+        print(self.name + " reduced to", self.hp, "HP.")
+
+
+print("HP bars set\up. Enter values for 10 enemies. If there is less than 10, set HP to 0 for empty slots:")
+mob1 = Mob("Enemy#1", int(input("Set HP for Enemy#1: ")))
+mob1.hp_set()
+
+mob2 = Mob("Enemy#2", int(input("Set HP for Enemy#2: ")))
+mob2.hp_set()
+
+mob3 = Mob("Enemy#3", int(input("Set HP for Enemy#3: ")))
+mob3.hp_set()
+
+mob4 = Mob("Enemy#4", int(input("Set HP for Enemy#4: ")))
+mob4.hp_set()
+
+mob5 = Mob("Enemy#5", int(input("Set HP for Enemy#5: ")))
+mob5.hp_set()
+
+mob6 = Mob("Enemy#6", int(input("Set HP for Enemy#6: ")))
+mob6.hp_set()
+
+mob7 = Mob("Enemy#7", int(input("Set HP for Enemy#7: ")))
+mob7.hp_set()
+
+mob8 = Mob("Enemy#8", int(input("Set HP for Enemy#8: ")))
+mob8.hp_set()
+
+mob9 = Mob("Enemy#3", int(input("Set HP for Enemy#9: ")))
+mob9.hp_set()
+
+mob10 = Mob("Enemy#10", int(input("Set HP for Enemy#10: ")))
+mob10.hp_set()
+
+print("HP bars set up successfully.")
+
+print('Enemy#1 HP', mob1.hp)
+print('Enemy#2 HP', mob2.hp)
+print('Enemy#3 HP', mob3.hp)
+print('Enemy#4 HP', mob4.hp)
+print('Enemy#5 HP', mob5.hp)
+print('Enemy#6 HP', mob6.hp)
+print('Enemy#7 HP', mob7.hp)
+print('Enemy#8 HP', mob8.hp)
+print('Enemy#9 HP', mob9.hp)
+print('Enemy#10 HP', mob10.hp)
+
+while mob1.hp > 0 or mob2.hp > 0 or mob3.hp > 0 or mob4.hp > 0 or mob5.hp > 0 or mob6.hp > 0 or mob7.hp > 0 or mob8.hp > 0 or mob9.hp > 0 or mob10.hp > 0:
+    target = int(input("Enter target number: "))
+    match target:
+        case 1:
+            mob1.oof()
+            mob1.results()
+        case 2:
+            mob2.oof()
+            mob2.results()
+        case 3:
+            mob3.oof()
+            mob3.results()
+        case 4:
+            mob4.oof()
+            mob4.results()
+        case 5:
+            mob5.oof()
+            mob5.results()
+        case 6:
+            mob6.oof()
+            mob6.results()
+        case 7:
+            mob7.oof()
+            mob7.results()
+        case 8:
+            mob8.oof()
+            mob8.results()
+        case 9:
+            mob9.oof()
+            mob9.results()
+        case 10:
+            mob10.oof()
+            mob10.results()
+        case _:
+            print("Enter a valid target number.")
+
+
+print("All enemies have been defeated!")
+input("Press any key to continue.")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HPtracker
 HP tracker for hit points based pen and paper RPGs
+
+It's designed for the Game Master to track enemies hit points during a combat encounter and skip all the quick math that has to be done during it.
+Instead the GM can use that focus and spared time to provide better narration and keep the fight entertaining from a storytelling perspective.
+The program is very simple and designed to be quick to use. It takes only a few seconds to set up the HP bars for each enemy. There can be up to 10 enemies but it's easy to reduce the number by giving redundant enemy slots "0" HP to signalize that they are not in use.
+After the HP setup you select enemies by their assigned number and then enter the amount of damage in hit points they received. The program will show how much HP they have left after the attack and wait for more input.
+If an enemy recovers HP you can enter a negative value into the input, this will increase the HP instead of reducing.
+When all enemy bars reach 0 or less, the program will let you know. So you don't even need to keep track of that.
